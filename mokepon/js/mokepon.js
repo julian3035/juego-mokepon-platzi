11b4e270881c6775b1260a427f4a79e81ff453bd
Julian Romero <juleo3035@gmail.com>
2023-08-15 14:26:26 -0500

    agregagndo addEventlistener en mokepon.js

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,23 @@
-function seleccionarMascotaJugador() {
-    alert("SELECCIONSTE TU MASCOTA")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById("hipodoge")
+    let inputCapipepo = document.getElementById("capipepo")
+    let inputRatigueya =document.getElementById("ratigueya")
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+    } else if (inputCapipepo.checked) {
+        alert("seleccionaste a Capipepo")
+    } else if (inputRatigueya.checked) {
+        alert("seleccionaste a Ratigueya")
+    } else {
+        alert("selecciona una mascota")
+    } 
+}
+
+
+
+window.addEventListener("load", iniciarJuego)
