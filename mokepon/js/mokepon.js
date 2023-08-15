2388493694e2f26f908602a141d972de6facbabb
Julian Romero <juleo3035@gmail.com>
2023-08-15 00:02:24 -0500

    agregando Mokepon.js

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert("SELECCIONSTE TU MASCOTA")
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota")
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
